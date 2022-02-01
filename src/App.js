2ee21a1e2d4a3b0dd66d22e89c797db066a9121b
Jeramiah Coffey <jerrygcoffey@gmail.com>
2022-02-01 02:00:31 -0600

    Delete feedback

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 
 function App() {
+  // eslint-disable-next-line
   const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
 
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
